5e6cc9c2ee646afa81d171c63857465f22d8574d
michikhi <mimoun.chikhi@capgemini-sogeti.com>
2014-08-18 14:24:19 +0200

    ok

--- jenkinsproject/src/test/java/com/test/firstjob/AppTest.java
+++ jenkinsproject/src/test/java/com/test/firstjob/AppTest.java
@@ -38,6 +38,6 @@ public class AppTest
     
     public void testGetString() {
     	App app = new App();
-    	assertEquals("Hello",app.getString("Helslo"));
+    	assertEquals("Hello",app.getString("Hello"));
     }
 }
